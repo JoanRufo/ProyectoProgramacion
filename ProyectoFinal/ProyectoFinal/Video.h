6b7c09fd7904186058adf340e7ce47d943ee4570
ArnauGarciaEscudero <60931551+ArnauGarciaEscudero@users.noreply.github.com>
2021-02-18 17:56:17 +0100

    VIDEO

--- ProyectoFinal/ProyectoFinal/Video.h
+++ ProyectoFinal/ProyectoFinal/Video.h
@@ -1,8 +1,18 @@
 #pragma once
+#include "SDL.h"
+
+
 class Video
 {
 public:
 	Video();
 	~Video();
+	void renderGraphic(int img, int posX, int posY, int width, int height);
+	void clearScreen(unsigned int color_key);
+	void updateScreen();
+	void waitTime(int ms);
+	void close();
+	SDL_Window* gWindow;
+	SDL_Surface* gScreenSurface;
 };
 
