b6de5224da26b39a32deae40dcbe2368b8e91051
JoanRufo <60931589+JoanRufo@users.noreply.github.com>
2021-04-25 20:06:45 +0200

    Personaje Clase

--- ProyectoFinal/ProyectoFinal/Personaje.cpp
+++ ProyectoFinal/ProyectoFinal/Personaje.cpp
@@ -1,1 +1,14 @@
 #include "Personaje.h"
+
+
+void Personaje::init()
+{
+}
+
+void Personaje::update()
+{
+}
+
+void Personaje::render()
+{
+}
--- ProyectoFinal/ProyectoFinal/Personaje.h
+++ ProyectoFinal/ProyectoFinal/Personaje.h
@@ -1,5 +1,16 @@
 #pragma once
+#include "SDL.h"
+#include <iostream>
+#include <stdio.h>
 class Personaje
 {
+	int Movx;
+	int Movy;
+
+public:
+	void init();
+	void update();
+	void render();
+
 };
 
