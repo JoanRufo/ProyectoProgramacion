38c35832c2b1da15023e3790c7e6e2a139bbdbcf
ArnauGarciaEscudero <60931551+ArnauGarciaEscudero@users.noreply.github.com>
2021-05-20 19:28:48 +0200

    sound manager 10%

--- ProyectoFinal/ProyectoFinal/SoundManager.h
+++ ProyectoFinal/ProyectoFinal/SoundManager.h
@@ -3,12 +3,31 @@
 #define SOUNDMANAGER
 
 #include <iostream>
+#include <map>
+#include <vector>
+#include "SDL.h"
+#include "SDL_mixer.h"
 
 class SoundManager
 {
 public:
-	SoundManager();
+	
 	~SoundManager();
+
+	void removeSound(const char* file);
+
+
+	Sint32 loadAndGetSoundID(const char* file);
+
+
+	std::string getSoundPathByID(Sint32 ID);
+
+
+	
+
+
+	
+	
 };
 #endif
 
