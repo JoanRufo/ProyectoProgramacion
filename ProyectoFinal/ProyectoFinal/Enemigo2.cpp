183a184dc085ada26914c009864bf76bb7585b9d
JoanRufo <60931589+JoanRufo@users.noreply.github.com>
2021-05-27 17:58:43 +0200

    Enemigo2

--- ProyectoFinal/ProyectoFinal/Enemigo2.cpp
+++ ProyectoFinal/ProyectoFinal/Enemigo2.cpp
@@ -14,10 +14,14 @@ Enemigo2::~Enemigo2()
 
 void Enemigo2::init()
 {
-	idDelEnemigo2 = mResourceManager->loadAndGetGraphicID("Imagenes/Enemigo1.png");
+	idDelEnemigo2 = mResourceManager->loadAndGetGraphicID("Imagenes/Enemigo2.png");
+	idDelEnemigo22 = mResourceManager->loadAndGetGraphicID("Imagenes/Enemigo2.png");
 	posX = 800;
 	posY = 300;
+	posX2 = 700;
+	posY2 = 500;
 	vel = 0.3;
+	vel2 = 0.1;
 }
 
 void Enemigo2::update()
@@ -27,11 +31,12 @@ void Enemigo2::update()
 void Enemigo2::render()
 {
 	mVideo->renderGraphic(idDelEnemigo2, posX, posY, 120, 100);
+	mVideo->renderGraphic(idDelEnemigo22, posX2, posY2, 200, 150);
 }
 
 void Enemigo2::movEnemigo()
 {
-
+	//PRIMER ENEMIGO
 	if (posX <= mPersonaje->Posx) {
 
 		posX = posX + 0.5 * vel;
@@ -53,6 +58,32 @@ void Enemigo2::movEnemigo()
 	else if (posY >= mPersonaje->Posy) {
 
 		posY = posY - 0.5 * vel;
+
+
+	}
+
+	//SEGUNDO ENEMIGO
+	if (posX2 <= mPersonaje->Posx) {
+
+		posX2 = posX2 + 0.5 * vel2;
+
+	}
+
+	else if (posX2 >= mPersonaje->Posx) {
+
+		posX2 = posX2 - 0.5 * vel2;
+
+	}
+
+	if (posY2 <= mPersonaje->Posy) {
+
+		posY2 = posY2 + 0.5 * vel2;
+
+	}
+
+	else if (posY2 >= mPersonaje->Posy) {
+
+		posY2 = posY2 - 0.5 * vel2;
 
 
 	}
